318b86f938f7237f17ca09690f419397c56bf30b
Kevin Raphael <kayraph3d@gmail.com>
2019-12-01 23:29:43 +0300

    added form validation

--- aknm/jss/scripts.js
+++ aknm/jss/scripts.js
@@ -22,17 +22,19 @@ function getName() {
 }
 
 //validation
-if (day <= 0 || day > 31)
-    alert("Oops!please enter a valid day");
-else if (month < 0 || month > 12)
-    alert("Oops!please enter a valid month");
+function validateForm() {
+    if (day <= 0 || day > 31)
+        alert("Oops!please enter a valid day");
+    else if (month < 0 || month > 12)
+        alert("Oops!please enter a valid month");
 
 
-var day = new Date(year + "/" + month + "/" + day);
+    var day = new Date(year + "/" + month + "/" + day);
 
-var birthDay = day.getDay();
-if (male.checked == true) {
-    alert("You were born on " + day + " and your akan name is " + maleNames)
-} else if (female.checked == true) {
-    alert("You were born on " + day + " and your akan name is " + femaleNames)
+    var birthDay = day.getDay();
+    if (male.checked == true) {
+        alert("You were born on " + daysOfWeek[birthDay] + " and your akan name is " + maleNames[birthDay])
+    } else if (female.checked == true) {
+        alert("You were born on " + daysOfWeek[birthDay] + " and your akan name is " + femaleNames[birthDay])
+    }
 }
