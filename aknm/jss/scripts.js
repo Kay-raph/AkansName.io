adee63d7996018afde04d8efbfebb8e44f91848c
Kevin Raphael <kayraph3d@gmail.com>
2019-12-01 23:18:50 +0300

    changed background color

--- aknm/jss/scripts.js
+++ aknm/jss/scripts.js
@@ -1,56 +1,38 @@
 //declaring arrays
-var daysOfWeek=[
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
+var daysOfWeek = [
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",
 ];
 
-var maleNames=[
-    "Kwasi",
-    "Kwadwo",
-    "Kwabena",
-    "Kwaku",
-    "Yaw",
-    "Kofi",
-    "Kwame",
+var maleNames = [
+    "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame",
 ];
 
-var femaleNames=[
-    "Akosua",
-    "Adwoa",
-    "Abenaa",
-    "Akua",
-    "Yaa",
-    "Afua",
-    "Ama",
+var femaleNames = [
+    "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama",
 ];
 
 //calling functions
-function getName(){
-var year=parseInt(document.getElementById("year").value);
-var month =parseInt(document.getElementById("month").value);
-var day=parseInt(document.getElementById("day").value);
-var male = document.getElementById("male");
-var female = document.getElementById("female");
+function getName() {
+    var year = parseInt(document.getElementById("year").value);
+    var month = parseInt(document.getElementById("month").value);
+    var day = parseInt(document.getElementById("day").value);
+    var male = document.getElementById("male");
+    var female = document.getElementById("female");
+    document.getElementById("myDate").innerHTML = alert;
+}
 
 //validation
-if(day<=0||day>31)
-alert("Oops!please enter a valid day");
-else if(month<0||month>12)
-alert("Oops!please enter a valid month");
+if (day <= 0 || day > 31)
+    alert("Oops!please enter a valid day");
+else if (month < 0 || month > 12)
+    alert("Oops!please enter a valid month");
 
 
 var day = new Date(year + "/" + month + "/" + day);
 
-var birthDay=day.getDay();
-if(male.checked==true){
-   alert("You were born on " +daysOfWeek[birthDay]+ " and your akan name is " + maleNames[birthDay]);
-}
-else if(female.checked==true){
-   alert("You were born on " +daysOfWeek[birthDay]+ " and your akan name is " + femaleNames[birthDay]);
-}
+var birthDay = day.getDay();
+if (male.checked == true) {
+    alert("You were born on " + day + " and your akan name is " + maleNames)
+} else if (female.checked == true) {
+    alert("You were born on " + day + " and your akan name is " + femaleNames)
 }
