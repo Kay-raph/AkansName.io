c2ce3b5a91acd91b2c94118eb542d120467b3d97
Kay-raph <52208503+Kay-raph@users.noreply.github.com>
2019-12-02 09:48:30 +0300

    Update scripts.js

--- aknm/jss/scripts.js
+++ aknm/jss/scripts.js
@@ -21,9 +21,9 @@ var female = document.getElementById("female");
 
 //validation
 if(day<=0||day>31)
-alert("Oops!please enter a valid day");
+alert("Sorry!Enter a valid day");
 else if(month<0||month>12)
-alert("Oops!please enter a valid month");
+alert("Sorry!Enter a valid month");
 
 
 var day = new Date(year + "/" + month + "/" + day);
